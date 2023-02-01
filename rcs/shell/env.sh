c95216f937b183d15e3932e1fb410b583b162373
Dan W Anderson <anderson.dan.w@gmail.com>
2023-02-01 16:44:06 +0000

    missed ag and fzf

--- rcs/shell/env.sh
+++ rcs/shell/env.sh
@@ -28,9 +28,9 @@ alias lt='ls -lrtG'
 export LSCOLORS=gxGxbxDxCxEgEdxbxgxcxd
 
 alias v="vim"
-# bindkey -v
+bindkey -v
 
-# prompt. This seems weird to mix-and-match $fg with $FG :shrug:. Looks like:
+# This seems weird to mix-and-match $fg with $FG :shrug:. Looks like:
 # [venv] user@ip pwd (git) \
 # time $
 setopt PROMPT_SUBST ; PS1='%{$fg[red]$(virtualenv_prompt_info) $fg[yellow]%n@%m $FG[075]%c$fg[green]$(__git_ps1 " (%s)")$reset_color%}
@@ -38,7 +38,29 @@ $FG[134]%*$FG[255] \$ '
 
 PATH="$HOME/bin:$PATH"
 
-alias d="docker"
-alias dps="docker ps --format='{{.Names}}' | sort"
-alias dltf="docker logs --tail=0 --follow"
-alias dclean="docker image prune && docker system prune -a"
+## ag helpers
+_AG_ARGS="--hidden \
+  --ignore .git \
+  --ignore .terraform \
+  --ignore terraform.tfstate* \
+  --color-match '1;35' \
+  --pager='less -RXF' \
+"
+
+# case-insensitive by default...
+# shellcheck disable=SC2139
+alias ag="ag -i $_AG_ARGS"
+
+# ... but occasionally i DO want case-sensitivity
+# shellcheck disable=SC2139
+alias AG="/usr/local/bin/ag $_AG_ARGS"
+
+## fzf helpers
+export FZF_DEFAULT_COMMAND='ag --nocolor --hidden --ignore .git -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS='
+--color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
+--color info:108,prompt:109,spinner:108,pointer:168,marker:168
+'
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
