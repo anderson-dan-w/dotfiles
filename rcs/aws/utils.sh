8a6eb83c83f45cd44073a3c7ef7e352a26bd4ee4
Dan W Anderson <anderson.dan.w@gmail.com>
2023-02-09 21:17:00 +0000

    fixup comments

--- rcs/aws/utils.sh
+++ rcs/aws/utils.sh
@@ -24,11 +24,9 @@ all-aws-bills () {
 # AWS EC2
 #############
 
-# ec2 will query for ec2 instances by name, with option wildcard
+# ec2 will query for ec2 instances by name, with trailing wildcard for convenience
 # returns tab-separated: instance-id, private-ip, name
-# examples on staging account
-#   ec2 'api*'  ## returns api-staging instances, since wildcard passed in
-#   ec2 api     ## returns nothing, since no machines named exactly "api"
+# eg: `ec2 dan` would SSM onto the "first" box named dan*, such as dan-dev-box
 ec2 () {
   aws ec2 describe-instances --filters="Name=tag:Name,Values=${1}*" | jq -r ' .Reservations[] .Instances[] | select(.State.Name == "running") | [.InstanceId, .PrivateIpAddress, (.Tags[]|select(.Key=="Name")|.Value)] | @tsv'
 }
@@ -38,10 +36,10 @@ ec2 () {
 #############
 
 # SSMs into either an instance id OR the first instance return from an "ec2" listing
-# switches to supplied user (ubuntu default) and into sigot-api dir for convenience
+# switches to supplied user (ubuntu default) and into $HOME dir for convenience
 #   ssm i-01234...   # specific instance
 #   ssm api-staging  # whichever api-staging machine ec2 returns first
-#   ssm dan-dev-box dan  # pops into /home/dan/sigopt-api as user=dan
+#   ssm dan-dev-box dan  # pops into /home/dan as user=dan
 ssm () {
   if [[ "${1}" =~ i-0 ]]
   then
@@ -52,5 +50,5 @@ ssm () {
   echo "instance:${instance}"
   SSM_USER="${2:-ubuntu}"
   instance_id="$( echo "${instance}" | cut -f1 )"
-  aws ssm start-session --target "${instance_id}" --parameters "command=cd /home/${SSM_USER}/sigopt-api; sudo su  ${SSM_USER}" --document-name "AWS-StartInteractiveCommand"
+  aws ssm start-session --target "${instance_id}" --parameters "command=cd /home/${SSM_USER}; sudo su  ${SSM_USER}" --document-name "AWS-StartInteractiveCommand"
 }
